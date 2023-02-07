48fe3e6cdc4089b88d21e331fc553199478e912f
Mangaka <123760132+Mangakaz@users.noreply.github.com>
2023-02-07 17:34:49 +0000

    Yea

---
+++ Project 1 .py
@@ -0,0 +1,21 @@
+
+# Add Sounds
+fitMedia(RD_UK_HOUSE_MAINBEAT_8, 1, 1, 5)
+fitMedia(RD_POP_SYNTHBASS_6, 2, 1, 9)
+fitMedia(YG_RNB_TAMBOURINE_1, 4, 1, 9)
+fitMedia(YG_FUNK_CONGAS_3, 5, 1, 5)
+fitMedia(YG_FUNK_HIHAT_2, 6, 5, 9)
+fitMedia(ENTREP_VOX_PHARRELL_BLKMAN, 5, 1, 8)
+fitMedia(AK_UNDOG_BASS_4, 4, 6, 9)
+fitMedia(AK_UNDOG_OOHS_AHHS_3, 3, 5, 7)
+fitMedia(CIARA_ROOTED_SYNTH_LEAD, 7, 5, 8)
+fitMedia(AK_UNDOG_STRINGS, 7, 4, 8)
+
+# Effects fade in
+setEffect(2, VOLUME,GAIN, -20, 1, 6, 5)
+
+# Fills
+fillA = "0---0-0-00--0000"
+fillB = "0--0--0--0--0-0-"
+fillC = "--000-00-00-0-00"
+makeBeat(OS_SNARE03, 3, 4, fillA)
